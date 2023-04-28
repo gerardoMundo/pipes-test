87b5dae985f219470f49ec9f84ad77b393cf8524
gerardoMundo <ger.mundo66@gmail.com>
2023-04-27 22:02:52 -0600

    Created basic pages architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { BasicPageComponent } from './products/pages/basic-page/basic-page.component';
+import { NumbersPageComponent } from './products/pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './products/pages/uncommon-page/uncommon-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BasicPageComponent, NumbersPageComponent, UncommonPageComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
