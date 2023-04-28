41da5fe5c73a1da7957cf7f072db723c9ef1ed64
gerardoMundo <ger.mundo66@gmail.com>
2023-04-27 21:55:13 -0600

    products routing module created

---
+++ src/app/products/products-routing/products-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingRoutingModule } from './products-routing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsRoutingRoutingModule
+  ]
+})
+export class ProductsRoutingModule { }
