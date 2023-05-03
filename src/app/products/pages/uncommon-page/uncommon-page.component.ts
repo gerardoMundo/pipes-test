eb2b192665c2d499de1b48566a3d0d84996f6040
gerardoMundo <ger.mundo66@gmail.com>
2023-05-02 19:19:37 -0600

    i18Select & i18Plural pipes added.

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,9 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class UncommonPageComponent {
+  public name = 'Gerardo';
+  public genre: 'male' | 'female' = 'male';
 
+  public clients: string[] = ['Maxi', 'Xavi', 'Chabelo', 'Hornz', 'Zlatahn'];
+  public clientsMap = {
+    '=0': 'no tenemos clientes.',
+    '=1': 'tenemos un clinte a la espera',
+    other: 'tenemos # clientes esperando',
+  };
+
+  public mapGenre = {
+    male: 'invitado',
+    female: 'invitada',
+  };
+
+  sendInvitation() {
+    this.name = 'Malis';
+    this.genre = 'female';
+  }
+
+  deleteClient() {
+    this.clients.shift();
+  }
 }
